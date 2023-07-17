4e6d46a158ae55cf9d3b31cae6345428c506af76
emosra <32685836+emosra@users.noreply.github.com>
2023-07-17 18:09:31 -0500

    Add error message for non matching passwords

--- sign-up-form/main.js
+++ sign-up-form/main.js
@@ -1,12 +1,16 @@
 const password = document.querySelector('#password')
 const confirmPassword = document.querySelector('#confirm-password')
 const submitBtn = document.querySelector('.submit-btn')
+const matchPassword = document.createElement('div')
+const signUpForm = document.querySelector('form')
 
 submitBtn.addEventListener('click', (e) => {
     
     if (password.value !== confirmPassword.value) {
         e.preventDefault()
-        console.log('no match') 
+        matchPassword.textContent = 'Passwords MUST be matching'
+        matchPassword.style.color = 'red'
+        signUpForm.appendChild(matchPassword)
         return
     }
 })
