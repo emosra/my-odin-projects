c47b25599b8bafd18f1c3270d0ce7e91286de052
emosra <32685836+emosra@users.noreply.github.com>
2023-07-21 12:49:31 -0500

    Create sidebar w/ buttons

--- library/main.js
+++ library/main.js
@@ -1,45 +1,32 @@
-const booksContainer = document.querySelector('.books-container')
+// const booksContainer = document.querySelector('.books-container')
 
-let myLibrary = [
-    // {
-    //     title: 'book 1',
-    //     author: 'author 1',
-    //     numOfPages: 150,
-    //     haveRead: false
-    // },
-    // {
-    //     title: 'book 2',
-    //     author: 'author 2',
-    //     numOfPages: 127,
-    //     haveRead: true
-    // },
-];
+// let myLibrary = [];
 
-function Book(title, author, numOfPages, haveRead) {
-    this.title = title;
-    this.author = author;
-    this.numOfPages = numOfPages;
-    this.haveRead = haveRead;
-}
+// function Book(title, author, numOfPages, haveRead) {
+//     this.title = title;
+//     this.author = author;
+//     this.numOfPages = numOfPages;
+//     this.haveRead = haveRead;
+// }
 
-Book.prototype.info = function() {
-    return `${this.title} by ${this.author}, ${this.numOfPages} pages`
-}
+// Book.prototype.info = function() {
+//     return `${this.title} by ${this.author}, ${this.numOfPages} pages`
+// }
 
-function addBookToLibrary(book) {
-    myLibrary.push(book);
-}
+// function addBookToLibrary(book) {
+//     myLibrary.push(book);
+// }
 
-const bookOne = new Book('book 1', 'author 1', 200, true)
-const bookTwo = new Book('book 2', 'author 2', 111, false)
+// const bookOne = new Book('book 1', 'author 1', 200, true)
+// const bookTwo = new Book('book 2', 'author 2', 111, false)
 
-addBookToLibrary(bookOne)
-addBookToLibrary(bookTwo)
+// addBookToLibrary(bookOne)
+// addBookToLibrary(bookTwo)
 
 
-for (const obj of myLibrary) {
-    const bookCard = document.createElement('div')
-    bookCard.textContent = obj.info();
-    booksContainer.appendChild(bookCard)
-    console.log(obj.info())
-}
+// for (const obj of myLibrary) {
+//     const bookCard = document.createElement('div')
+//     bookCard.textContent = obj.info();
+//     booksContainer.appendChild(bookCard)
+//     console.log(obj.info())
+// }
